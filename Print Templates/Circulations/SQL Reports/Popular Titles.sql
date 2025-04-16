35f7440240837ea898e70f479bb586bfb8dc8459
Tillamook County Library <tcl-apps@tillabook.org>
2025-04-16 15:02:14 -0700

    Popular titles 4/16/2025

---
+++ Print Templates/Circulations/SQL Reports/Popular Titles.sql
@@ -0,0 +1,10 @@
+SELECT count(s.datetime) AS circs, b.biblionumber, b.title, b.author
+FROM statistics s
+JOIN items i ON (i.itemnumber=s.itemnumber)
+LEFT JOIN biblio b ON (b.biblionumber=i.biblionumber)
+WHERE s.datetime BETWEEN <<Begin Date|date>> AND <<End Date|date>>
+    AND s.itemnumber IS NOT NULL 
+    AND s.type IN ('issue','renew') 
+    AND i.homebranch IN ('TILLMAIN', 'TILLBAY', 'TILLGAR', 'TILLROCK', 'TILLMANZ', 'TILLPAC')
+GROUP BY b.biblionumber
+ORDER BY circs DESC LIMIT 100
