ee52a5b358ab5762847f300f78cf455a5b0e86ea
Tillamook County Library <tcl-apps@tillabook.org>
2025-04-16 15:05:46 -0700

    new update

--- SQL Reports/Popular Titles.sql
+++ SQL Reports/Popular Titles.sql
@@ -5,6 +5,6 @@ LEFT JOIN biblio b ON (b.biblionumber=i.biblionumber)
 WHERE s.datetime BETWEEN <<Begin Date|date>> AND <<End Date|date>>
     AND s.itemnumber IS NOT NULL 
     AND s.type IN ('issue','renew') 
-    AND i.homebranch IN ('TILLMAIN', 'TILLBAY', 'TILLGAR', 'TILLROCK', 'TILLMANZ', 'TILLPAC')
+    AND i.homebranch IN ('TILLMAIN', 'TILLBAY', 'TILLGAR', 'TILLROCK', 'TILLMANZ', 'TILLPAC', 'TILLBKM', 'TILLOUT')
 GROUP BY b.biblionumber
 ORDER BY circs DESC LIMIT 100
